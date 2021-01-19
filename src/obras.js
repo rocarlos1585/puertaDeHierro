a636ce17fb2099bcd0ec20bdd8867ebe79a150c8
rocarlos1585 <roberto.karlos.lopez@gmail.com>
2021-01-19 13:53:53 -0600

    se siguieron agregando fotos al apartado de obras

--- src/obras.js
+++ src/obras.js
@@ -24,8 +24,15 @@ class Obras extends Component{
                         <h3 className="vertical-timeline-element-title">ENTREGA DE UNIFORMES</h3>
                         <h4 className="vertical-timeline-element-subtitle">Administración</h4>
                         <p>
-                        En compromiso con todos nuestros trabajoders se les hizo entrega de nuevos uniformes.
+                            En compromiso con todos nuestros trabajoders se les hizo entrega de nuevos uniformes.
                         </p>
+
+                        <div className="imagesContainer">
+                            <img className="" src="http://puertadehierroac.com/imagenes/entregaUniformes/Imagen17.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/entregaUniformes/Imagen18.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/entregaUniformes/Imagen19.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/entregaUniformes/Imagen20.png"/>
+                        </div>
                     </VerticalTimelineElement>
 
 
@@ -47,12 +54,11 @@ class Obras extends Component{
                             Panorama general de la zona iluminada del fraccionamiento sobre el camellón del boulevard
                         </p>
 
-                        <div>
-                            <img/>
-                            <img/>
-                            <img/>
-                            <img/>
-                            
+                        <div className="imagesContainer">
+                            <img className="" src="http://puertadehierroac.com/imagenes/lucesBoulevard/Imagen9.jpg"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/lucesBoulevard/Imagen10.jpg"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/lucesBoulevard/Imagen11.jpg"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/lucesBoulevard/Imagen12.jpg"/>
                         </div>
                     </VerticalTimelineElement>
 
@@ -68,8 +74,15 @@ class Obras extends Component{
                         <h3 className="vertical-timeline-element-title">MTTO. A PALMERAS</h3>
                         <h4 className="vertical-timeline-element-subtitle">Jardineria</h4>
                         <p>
-                        User Experience, Visual Design
+                            Descripcion pendiente....
                         </p>
+
+                        <div className="imagesContainer">
+                            <img className="" src="http://puertadehierroac.com/imagenes/mttoPalmeras/Imagen53.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/mttoPalmeras/Imagen54.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/mttoPalmeras/Imagen55.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/mttoPalmeras/Imagen56.png"/>
+                        </div>
                     </VerticalTimelineElement>
 
 
@@ -86,6 +99,13 @@ class Obras extends Component{
                         <p>
                             Adquisición de vehículos toyota yaris
                         </p>
+
+                        <div className="imagesContainer">
+                            <img className="" src="http://puertadehierroac.com/imagenes/parqueVehicular/Imagen24.jpg"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/parqueVehicular/Imagen170.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/parqueVehicular/Imagen25.jpg"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/parqueVehicular/Imagen173.png"/>
+                        </div>
                     </VerticalTimelineElement>
 
 
@@ -101,8 +121,15 @@ class Obras extends Component{
                         <h3 className="vertical-timeline-element-title">PODA DE ARBOLES SOBRE AV. ACUEDUCTO</h3>
                         <h4 className="vertical-timeline-element-subtitle">Jardineria</h4>
                         <p>
-                        User Experience, Visual Design
+                            Descripcion pendiente....
                         </p>
+
+                        <div className="imagesContainer">
+                            <img className="" src="http://puertadehierroac.com/imagenes/podaArboles/Imagen62.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/podaArboles/Imagen63.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/podaArboles/Imagen64.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/podaArboles/Imagen65.png"/>
+                        </div>
                     </VerticalTimelineElement>
 
 
@@ -120,6 +147,14 @@ class Obras extends Component{
                         <p>
                             Se adquirio nuevo equipo para el departamento de monitoreo entre monitores y mobiliario.
                         </p>
+
+                        
+                        <div className="imagesContainer">
+                            <img className="" src="http://puertadehierroac.com/imagenes/mejoraMonitoreo/Imagen38.jpg"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/mejoraMonitoreo/imagen1.PNG"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes//mejoraMonitoreo/imagen2.PNG"/>
+                            
+                        </div>
                     </VerticalTimelineElement>
 
 
@@ -136,14 +171,61 @@ class Obras extends Component{
                         <h3 className="vertical-timeline-element-title">FERTILIZACIÓN A PLANTAS DE HORNATO</h3>
                         <h4 className="vertical-timeline-element-subtitle">Jardineria</h4>
                         <p>
-                        User Experience, Visual Design
+                            Descripcion pendiente....
                         </p>
+
+                        <div className="imagesContainer">
+                            <img className="" src="http://puertadehierroac.com/imagenes/plantasHornato/Imagen66.jpg"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/plantasHornato/Imagen67.jpg"/>                            
+                        </div>
                     </VerticalTimelineElement>
 
 
 
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#964824', color: 'white' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #964824' }}
+                        date="2020"
+                        iconStyle= {{background: '#964824', color: '#fff', textAlign:'center', paddingTop:'1%'}}
+                        icon={<Icon size="big" name='chart bar outline' />}
+                    >
+                        <h3 className="vertical-timeline-element-title">TRABAJOS DE TOTALPLAY</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Obras e Inversión</h4>
+                        <p>
+                            Con el objetivo de mejorar la covertura del servicio se realizaron trabajos en la infraestructura de TotalPlay en los cotos de Barcelona, Sevilla, Galicia, León y Andalucía 
+                        </p>
+
+                        <div className="imagesContainer">
+                            <img className="" src="http://puertadehierroac.com/imagenes/trabajosTotalplay/Imagen34.jpg"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/trabajosTotalplay/Imagen35.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/trabajosTotalplay/Imagen36.jpg"/>
+
+                        </div>
+                    </VerticalTimelineElement>
 
 
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#24962e', color: 'white' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #24962e' }}
+                        date="2020"
+                        iconStyle= {{background: '#24962e', color: '#fff', textAlign:'center', paddingTop:'1%'}}
+                        icon={<Icon size="big" name='tree' />}
+                    >
+                        <h3 className="vertical-timeline-element-title">NUTRIENTES EN ÁRBOLES</h3>
+                        <h4 className="vertical-timeline-element-subtitle">Jardineria</h4>
+                        <p>
+                            En el mes de junio se aplicaron nutrienes y fertilizantes a todos los árboles.
+                        </p>
+
+                        <div className="imagesContainer">
+                            <img className="" src="http://puertadehierroac.com/imagenes/nutrientesArboles/Imagen68.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/nutrientesArboles/Imagen69.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/nutrientesArboles/Imagen70.png"/>
+                            <img className="" src="http://puertadehierroac.com/imagenes/nutrientesArboles/Imagen71.png"/>
+                        </div>
+                    </VerticalTimelineElement>
 
 
                 </VerticalTimeline>
